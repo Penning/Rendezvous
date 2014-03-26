3e9624858c84118e72506015bbaf8db74695e672
Sumedha <pramods@umich.edu>
2014-03-25 20:47:59 -0400

    Added user class to save friend lists to

---
+++ Rendezvous/CurrentUser.h
@@ -0,0 +1,13 @@
+//
+//  CurrentUser.h
+//  Rendezvous
+//
+//  Created by Sumedha Pramod on 3/25/14.
+//  Copyright (c) 2014 Penning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CurrentUser : NSObject
+
+@end
