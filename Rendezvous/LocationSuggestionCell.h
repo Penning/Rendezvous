ef48ae2368234379f8dafc8dd3e7b67e9bb1b842
Sumedha <pramods@umich.edu>
2014-04-10 16:00:35 -0400

    Added category images to locationView cells

--- Rendezvous/LocationSuggestionCell.h
+++ Rendezvous/LocationSuggestionCell.h
@@ -14,7 +14,8 @@
 @property (strong, nonatomic) IBOutlet UILabel *distance;
 @property (strong, nonatomic) IBOutlet UILabel *address;
 @property (strong, nonatomic) IBOutlet UIImageView *image;
+@property (strong, nonatomic) IBOutlet UIImageView *categoryImage;
 
-- (void)initCellDisplay:(MeetingLocation *) meeting;
+- (void)initCellDisplay:(MeetingLocation *) meeting :(NSString *) reason;
 
 @end
--- Rendezvous/LocationViewController.h
+++ Rendezvous/LocationViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import <Parse/Parse.h>
+#import "Meeting.h"
 
 @interface LocationViewController : UIViewController
 
@@ -17,5 +18,7 @@
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSMutableArray *suggestions;
+
+@property (strong, nonatomic) Meeting *meeting;
 
 @end
